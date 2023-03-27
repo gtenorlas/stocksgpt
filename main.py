3d852d0904cfb15a6fb5ff85813e1896076e3dd4
Gene Tenorlas <tenorlas@sheridancollege.ca>
2023-03-27 11:23:15 -0400

    implement pandas to analyze data and matplotlib to create graphs

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+# pip install requests
+# pip install python-dotenv
+# pip install pandas
 import requests
 from dotenv import load_dotenv
 import os
+import pandas as pd
+import matplotlib.pyplot as plt
 
 load_dotenv()
 
@@ -14,4 +19,16 @@ url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&sy
 
 response = requests.get(url)
 data = response.json()
-//print (data)
+#print (data)
+
+
+#use pandas to analyze data received and use matplotlib to plot the data
+df = pd.DataFrame.from_dict(data['Time Series (Daily)'], orient='index')
+df = df[['4. close']].astype(float)
+
+""" plt.plot(df.index, df['4. close'])
+plt.title('AAPL Stock Price')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
+ """
